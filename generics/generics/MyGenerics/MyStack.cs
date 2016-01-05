861de6694a93db9e6bc236f4563d41868c722498
Vitaliy Ivasenko <vivasenko@corevalue.net>
2016-01-05 09:49:31 +0200

    Generics: Stack rev.1

---
+++ generics/generics/MyGenerics/MyStack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generics.MyGenerics
+{
+    public class MyStack<T>
+    {
+        private int _position;
+
+        private T[] _data = new T[1];
+
+        public void Push(T obj)
+        {
+            if (_position == _data.Length)
+            {
+                var tempData = new T[_data.Length + 1];
+                _data.CopyTo(tempData, 0);
+                _data = tempData;
+            }
+
+            _data[_position++] = obj;
+        }
+
+        public T Pop()
+        {
+            if (_position - 1 < 0)
+                throw new ArgumentNullException();
+
+            var res = _data[--_position];
+            var tempData = new T[_data.Length - 1];
+            tempData = _data.Take(_data.Length - 1).ToArray();
+            _data = tempData;
+
+            return res;
+        }
+
+        public T Peek()
+        {
+            if (_position - 1 < 0)
+                throw new ArgumentNullException();
+
+            return _data[_position - 1];
+        }
+    }
+}
---
+++ generics/generics/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using generics.MyGenerics;
+
+namespace generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new MyStack<int>();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(5);
+
+            try
+            {
+                Console.WriteLine(stack.Peek());
+
+                Console.WriteLine(stack.Pop());
+                Console.WriteLine(stack.Pop());
+                Console.WriteLine(stack.Pop());
+
+                Console.WriteLine(stack.Peek());
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("empty stack!!!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
